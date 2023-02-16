492a62139be37daa4905c7c07688cdbeb4c5aa85
Dhruv Desai <dhruvdesai222@gmail.com>
2023-02-16 08:34:17 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Function
+
+# f(x) = x ^ 2 (Mathematical function) f(2)= 4
+
+# def <FUNCTION NAME> (Input):
+# CALCULATION
+# return RESULT
+
+def f1(x):
+    return x * x
+
+# more preferred compare to f1
+def f2(x):
+    print('step1: Initial step')
+    y = x * x
+    print('step 2: calculating y')
+    return y
+
+# To run the function/ to call the fuction FUNCTION_NAME(Parameter= value)
+# To call function: write the function name and assign the input if applicable
+
+result = f2(x=2)
+print(result)
+
+
+
+
